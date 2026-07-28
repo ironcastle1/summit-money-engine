68b8eb974fd8dfba6ab213013205552e4b3f0f55
ironcastle1 <alessandroxlvii@gmail.com>
2026-07-28 17:43:27 +0100

    Delete server directory

--- server/core/scheduler.js
+++
@@ -1,9 +0,0 @@
-const { refreshState } = require("./stateStore");
-let started = false;
-function startScheduler() {
-  if (started) return;
-  started = true;
-  refreshState(true).catch(err => console.warn("Initial refresh failed", err.message));
-  setInterval(() => refreshState(true).catch(err => console.warn("Scheduled refresh failed", err.message)), 5 * 60 * 1000);
-}
-module.exports = { startScheduler };
