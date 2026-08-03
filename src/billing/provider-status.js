a639edf89ab2cd876424e2f55d374c08a88b3881
ironcastle1 <alessandroxlvii@gmail.com>
2026-08-03 16:03:09 +0100

    Delete src directory

--- src/billing/provider-status.js
+++
@@ -1,2 +0,0 @@
-export const BILLING_STATES = Object.freeze({ READY: 'READY', NOT_CONFIGURED: 'NOT_CONFIGURED', ERROR: 'ERROR' });
-export function providerHealth(id, configured, extra = {}) { return { id, state: configured ? BILLING_STATES.READY : BILLING_STATES.NOT_CONFIGURED, configured: Boolean(configured), ...extra }; }
