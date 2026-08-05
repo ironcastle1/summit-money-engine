9bb9fd1614baac6cb81c70389446717f87e88025
ironcastle1 <alessandroxlvii@gmail.com>
2026-08-05 12:53:41 +0100

    Delete src directory

--- src/services/audit-service.js
+++
@@ -1,6 +0,0 @@
-import { createAuditEvent } from '../domain/accounts/audit-event.js';
-export class AuditService {
-  constructor(repository) { this.repository = repository; }
-  async record(input) { return this.repository.append(createAuditEvent(input)); }
-  async list(options) { return this.repository.list(options); }
-}
