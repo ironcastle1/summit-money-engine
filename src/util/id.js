4291d029f3a71fad3ccd21c7623d7a243ca4d6a1
ironcastle1 <alessandroxlvii@gmail.com>
2026-09-09 12:54:13 +0100

    MERLIN CNC V9.8 fix image isolation and Fusion DXF export

--- src/util/id.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto';
-
-export function id(prefix) {
-  return `${prefix}-${crypto.randomUUID()}`;
-}
-
-export function sha256(buffer) {
-  return crypto.createHash('sha256').update(buffer).digest('hex');
-}
